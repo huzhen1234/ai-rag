cd1c03aac569abb3177ebf86412b4ba192fe6fa1
huzhen <huzhen@bestpay.com.cn>
2025-08-04 10:50:45 +0800

    ollama rag 知识库简化

--- rag-app/src/main/java/com/hutu/config/AiModelConfig.java
+++ rag-app/src/main/java/com/hutu/config/AiModelConfig.java
@@ -1,7 +1,9 @@
 package com.hutu.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,9 +11,11 @@ import org.springframework.context.annotation.Configuration;
 public class AiModelConfig {
 
     @Bean
-    public ChatClient chatClient(OllamaChatModel ollamaChatModel) {
+    public ChatClient chatClient(OllamaChatModel ollamaChatModel, VectorStore pgVectorStore) {
         return ChatClient
                 .builder(ollamaChatModel)
+                // 添加了这样的配置就不需要 OllamaController askQuestion "/rag/ask" 写这么复杂的接口了。
+                .defaultAdvisors(new QuestionAnswerAdvisor(pgVectorStore))
                 .build();
     }
 }
